3ea1830fc4bb6b07c003b76b4e7669ef90dcdcc0
Manas Kumar <manaskumar@example.com>
2026-01-18 01:27:24 +0530

    fix: added frontend api endpoint

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,10 @@ app = FastAPI(title="HealthCareAI API")
 # Configure CORS for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://health-care-ai-three.vercel.app/",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
